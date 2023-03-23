da07ec6eb72ad203714030990e3ba78d9d3837fe
Abu Al Shahriar Rifat <rifatdiu2686@gmail.com>
2023-03-23 15:18:27 +0600

    Boolean section has completed

---
+++ 23_03_2023.py
@@ -0,0 +1,41 @@
+name="Rifat";
+for i in range(len(name)):
+    print(name[i]);
+
+print(7>3);
+print(7<3);
+
+name='Rifat';
+myNae='shahriar';
+print(name==myNae);
+
+isMarries='true';
+print(bool(isMarries));
+print('Cats' !='Dogs');
+com1=200;
+com2=300;
+com=com1==com2
+print(bool(com))
+com3='';
+print(bool(com3));
+print(19>45);
+
+print(30==30);
+a=10
+b=7
+c=6
+print('\n');
+print(a>=b);
+print(a>=c);
+
+print('\n')
+
+name='Abu Al Shahriar Rifat'
+print('Abu' in name);
+print('mn' in name);
+print("Shahriar" not in name);
+print('Abu' is name);
+print('Abu ' is not name);
+
+
+
